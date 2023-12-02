c88539676a65ce866e588787cbe938d767c6c326
Travis Dempsey <travis.dempsey@jumpcloud.com>
2023-12-02 12:52:37 -0600

    day 2

--- calendar/day2.go
+++ calendar/day2.go
@@ -2,18 +2,76 @@ package calendar
 
 import (
 	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func init() {
 	registry["Day2"] = Day2
 }
 
+var maximum = map[string]int{
+	"red": 12,
+	"green": 13,
+	"blue": 14,
+}
+
 func Day2(input *bufio.Scanner, part int) (result int) {
-	if part == 1 {
-
-	} else {
-
+	games := make(map[int][]map[string]int)
+	for input.Scan() {
+		info := strings.Split(input.Text(), ":")
+		re := regexp.MustCompile(`^Game (\d+)`)
+		gameInfo := re.FindStringSubmatch(info[0])
+		gameId, _ := strconv.Atoi(gameInfo[1])
+		revealed := strings.Split(info[1], ";")
+		games[gameId] = []map[string]int{}
+		for _, set := range revealed {
+			drawn := make(map[string]int)
+			color := regexp.MustCompile(`(?P<count>\d+) (?P<color>blue|green|red)`)
+			colorInfo := color.FindAllStringSubmatch(set, -1)
+			for _, selected := range colorInfo {
+				count, _ := strconv.Atoi(selected[1])
+				drawn[selected[2]] = count
+			}
+			games[gameId] = append(games[gameId], drawn)
+			log.Debug(drawn)
+		}
 	}
-
-	return
+	if part == 1 {
+		for id, drawings := range games {
+			possible := true
+			for _, drawn := range drawings {
+				if drawn["red"] > maximum["red"] || drawn["green"] > maximum["green"] || drawn["blue"] > maximum["blue"] {
+					possible = false
+					log.Debug("Impossible")
+					log.Debug(id)
+					log.Debug(drawn)
+					break
+				}
+			}
+			if possible {
+				result += id
+			}
+		}
+		return
+	} else {
+		for _, drawings := range games {
+			powerSet := map[string]int{
+				"red": 0,
+				"green": 0,
+				"blue": 0,
+			}
+			for _, drawn := range drawings {
+				for _, color := range []string{"red", "green", "blue"} {
+					if drawn[color] > powerSet[color] {
+						powerSet[color] = drawn[color]
+					}
+				}
+			}
+			power := powerSet["red"] * powerSet["green"] * powerSet["blue"]
+			result += power
+		}
+		return
+	}
 }
--- calendar/day2_test.go
+++ calendar/day2_test.go
@@ -11,14 +11,24 @@ import (
 
 var _ = Describe("Day2", func() {
 	It("part 1 should be correct", func() {
-		input := bufio.NewScanner(strings.NewReader(``))
-		result := 0
+		input := bufio.NewScanner(strings.NewReader(`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`))
+		result := 8
 		Expect(calendar.Day2(input, 1)).To(Equal(result))
 	})
 
 	It("part 2 should be correct", func() {
-		input := bufio.NewScanner(strings.NewReader(``))
-		result := 0
+		input := bufio.NewScanner(strings.NewReader(`Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+`))
+		result := 2286
 		Expect(calendar.Day2(input, 2)).To(Equal(result))
 	})
 })
