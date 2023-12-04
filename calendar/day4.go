f4cdb3c06cf3ffca24c60c3f67ae07f1f4b918c2
Travis Dempsey <travis.dempsey@jumpcloud.com>
2023-12-04 00:07:21 -0600

    day4

--- calendar/day4.go
+++ calendar/day4.go
@@ -2,18 +2,66 @@ package calendar
 
 import (
 	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func init() {
 	registry["Day4"] = Day4
 }
 
+type Card struct {
+	id   int
+	wins int
+}
+
 func Day4(input *bufio.Scanner, part int) (result int) {
-	if part == 1 {
-
-	} else {
-
+	totalCards := []*Card{}
+	for input.Scan() {
+		info := strings.Split(input.Text(), ":")
+		re := regexp.MustCompile(`^Card\s+(\d+)`)
+		cardInfo := re.FindStringSubmatch(info[0])
+		cardId, _ := strconv.Atoi(cardInfo[1])
+		card := &Card{ id: cardId, wins: 0 }
+		numbers := strings.Split(info[1], "|")
+		picks := strings.Fields(numbers[0])
+		winners := strings.Fields(numbers[1])
+		points := 0
+		for _, pick := range picks {
+			for _, winner := range winners {
+				if pick == winner {
+					card.wins++
+					if points == 0 {
+						points = 1
+					} else {
+						points = points * 2
+					}
+				}
+			}
+		}
+		result += points
+		totalCards = append(totalCards, card)
 	}
-
-	return
+	if part == 1 {
+		return
+	} else {
+		result = 0
+		process := map[int]int{}
+		for _, card := range totalCards {
+			process[card.id] = 1
+		}
+		for _, card := range totalCards {
+			log.Infof("Processing card %d with %d wins %d times", card.id, card.wins, process[card.id])
+			for c := 0; c < process[card.id]; c++ {
+				for i := 0; i < card.wins; i++ {
+					process[card.id + i + 1]++
+				}
+			}
+		}
+		for _, proc := range process {
+			result += proc
+		}
+		return
+	}
 }
--- calendar/day4_test.go
+++ calendar/day4_test.go
@@ -11,14 +11,26 @@ import (
 
 var _ = Describe("Day4", func() {
 	It("part 1 should be correct", func() {
-		input := bufio.NewScanner(strings.NewReader(``))
-		result := 0
+		input := bufio.NewScanner(strings.NewReader(`Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`))
+		result := 13
 		Expect(calendar.Day4(input, 1)).To(Equal(result))
 	})
 
 	It("part 2 should be correct", func() {
-		input := bufio.NewScanner(strings.NewReader(``))
-		result := 0
+		input := bufio.NewScanner(strings.NewReader(`Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+`))
+		result := 30
 		Expect(calendar.Day4(input, 2)).To(Equal(result))
 	})
 })
