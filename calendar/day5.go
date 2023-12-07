ab151dc68c0fe25e1b5cc4ad6a56afe34227a657
Travis Dempsey <travis.dempsey@jumpcloud.com>
2023-12-07 12:09:58 -0600

    day 5

--- calendar/day5.go
+++ calendar/day5.go
@@ -10,44 +10,43 @@ func init() {
 	registry["Day5"] = Day5
 }
 
-var seeds = []int{ 79, 14, 55, 13 }
-//var seeds = []int{ 919339981, 562444630, 3366006921, 67827214, 1496677366, 101156779, 4140591657, 5858311, 2566406753, 71724353, 2721360939, 35899538, 383860877, 424668759, 3649554897, 442182562, 2846055542, 49953829, 2988140126, 256306471 }
+// var seeds = []int{ 79, 14, 55, 13 }
+var seeds = []int{ 919339981, 562444630, 3366006921, 67827214, 1496677366, 101156779, 4140591657, 5858311, 2566406753, 71724353, 2721360939, 35899538, 383860877, 424668759, 3649554897, 442182562, 2846055542, 49953829, 2988140126, 256306471 }
 
 func Day5(input *bufio.Scanner, part int) (result int) {
 
-	input = bufio.NewScanner(strings.NewReader(`seed-to-soil map:
-50 98 2
-52 50 48
+//	input = bufio.NewScanner(strings.NewReader(`seed-to-soil map:
+// 50 98 2
+// 52 50 48
 
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
+// soil-to-fertilizer map:
+// 0 15 37
+// 37 52 2
+// 39 0 15
 
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
+// fertilizer-to-water map:
+// 49 53 8
+// 0 11 42
+// 42 0 7
+// 57 7 4
 
-water-to-light map:
-88 18 7
-18 25 70
+// water-to-light map:
+// 88 18 7
+// 18 25 70
 
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
+// light-to-temperature map:
+// 45 77 23
+// 81 45 19
+// 68 64 13
 
-temperature-to-humidity map:
-0 69 1
-1 0 69
+// temperature-to-humidity map:
+// 0 69 1
+// 1 0 69
 
-humidity-to-location map:
-60 56 37
-56 93 4
-`))
-
+// humidity-to-location map:
+// 60 56 37
+// 56 93 4
+// `))
 
 	collectRange := false
 	var rangeKey string
@@ -63,7 +62,7 @@ humidity-to-location map:
 			seedRanges[i/2] = append(seedRanges[i/2], seedRanges[i/2][0] + seed - 1)
 		}
 	}
-	log.Debug(seedRanges)
+	log.Info(seedRanges)
 	for input.Scan() {
 		if !collectRange {
 			if strings.Contains(input.Text(), "seed-to-soil") {
@@ -106,93 +105,86 @@ humidity-to-location map:
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Mapping soil %d to fertilizer", seed)
+			// log.Infof("Mapping soil %d to fertilizer", seed)
 			for _, rang := range rangeMap["soil-to-fertilizer"] {
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Mapping fertilizer %d to water", seed)
+			// log.Infof("Mapping fertilizer %d to water", seed)
 			for _, rang := range rangeMap["fertilizer-to-water"] {
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Mapping water %d to light", seed)
+			// log.Infof("Mapping water %d to light", seed)
 			for _, rang := range rangeMap["water-to-light"] {
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Mapping light %d to temperature", seed)
+			// log.Infof("Mapping light %d to temperature", seed)
 			for _, rang := range rangeMap["light-to-temperature"] {
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Mapping temperature %d to humidity", seed)
+			// log.Infof("Mapping temperature %d to humidity", seed)
 			for _, rang := range rangeMap["temperature-to-humidity"] {
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Mapping humidity %d to location", seed)
+			// log.Infof("Mapping humidity %d to location", seed)
 			for _, rang := range rangeMap["humidity-to-location"] {
 				dstRangeStart, _ := strconv.Atoi(rang[0])
 				srcRangeStart, _ := strconv.Atoi(rang[1])
 				length, _ := strconv.Atoi(rang[2])
-				log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+				// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
 				if seed >= srcRangeStart && seed < (srcRangeStart + length) {
-					log.Info("mapped")
 					seed = dstRangeStart - srcRangeStart + seed
 					break
 				}
 			}
 
-			log.Infof("Final location %d", seed)
+			// log.Infof("Final location %d", seed)
 			if seed < result {
 				result = seed
 			}
@@ -200,47 +192,140 @@ humidity-to-location map:
 
 		return
 	} else {
-		for _, sRange := range seedRanges {
-			log.Infof("Mapping seed range %v to soil", sRange)
-			next := compareRanges(sRange, rangeMap["seed-to-soil"])
-			log.Infof("Soil ranges to process %v", next)
-
-			for _, sr := range next {
-				next = compareRanges(sr, rangeMap["soil-to-fertilizer"])
-				log.Infof("Fertilizer ranges to process %v", next)
-			}
-
-			for _, sr := range next {
-				next = compareRanges(sr, rangeMap["fertilizer-to-water"])
-				log.Infof("Water ranges to process %v", next)
-			}
-
-			for _, sr := range next {
-				next = compareRanges(sr, rangeMap["water-to-light"])
-				log.Infof("Light ranges to process %v", next)
-			}
-
-			for _, sr := range next {
-				next = compareRanges(sr, rangeMap["light-to-temperature"])
-				log.Infof("Temperature ranges to process %v", next)
-			}
-
-			for _, sr := range next {
-				next = compareRanges(sr, rangeMap["temperature-to-humidity"])
-				log.Infof("Humidity ranges to process %v", next)
-			}
-
-			for _, sr := range next {
-				next = compareRanges(sr, rangeMap["humidity-to-location"])
-				log.Infof("Location ranges %v", next)
-				for _, r := range next {
-					if r[0] < result {
-						result = r[0]
+		for _, seedRange := range seedRanges {
+			for s := seedRange[0]; s <= seedRange[1]; s++ {
+				seed := s
+				// log.Infof("Mapping seed %d to soil", seed)
+				for _, rang := range rangeMap["seed-to-soil"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
 					}
+				}
+
+				// log.Infof("Mapping soil %d to fertilizer", seed)
+				for _, rang := range rangeMap["soil-to-fertilizer"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
+					}
+				}
+
+				// log.Infof("Mapping fertilizer %d to water", seed)
+				for _, rang := range rangeMap["fertilizer-to-water"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
+					}
+				}
+
+				// log.Infof("Mapping water %d to light", seed)
+				for _, rang := range rangeMap["water-to-light"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
+					}
+				}
+
+				// log.Infof("Mapping light %d to temperature", seed)
+				for _, rang := range rangeMap["light-to-temperature"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
+					}
+				}
+
+				// log.Infof("Mapping temperature %d to humidity", seed)
+				for _, rang := range rangeMap["temperature-to-humidity"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
+					}
+				}
+
+				// log.Infof("Mapping humidity %d to location", seed)
+				for _, rang := range rangeMap["humidity-to-location"] {
+					dstRangeStart, _ := strconv.Atoi(rang[0])
+					srcRangeStart, _ := strconv.Atoi(rang[1])
+					length, _ := strconv.Atoi(rang[2])
+					// log.Infof("checking if between %d and %d", srcRangeStart, srcRangeStart + length)
+					if seed >= srcRangeStart && seed < (srcRangeStart + length) {
+						seed = dstRangeStart - srcRangeStart + seed
+						break
+					}
+				}
+
+				// log.Infof("Final location %d", seed)
+				if seed < result {
+					result = seed
 				}
 			}
 		}
-		return
+		return result
+		// for _, sRange := range seedRanges {
+		//	log.Infof("Mapping seed range %v to soil", sRange)
+		//	next := compareRanges(sRange, rangeMap["seed-to-soil"])
+		//	log.Infof("Soil ranges to process %v", next)
+
+		//	for _, sr := range next {
+		//		next = compareRanges(sr, rangeMap["soil-to-fertilizer"])
+		//		log.Infof("Fertilizer ranges to process %v", next)
+		//	}
+
+		//	for _, sr := range next {
+		//		next = compareRanges(sr, rangeMap["fertilizer-to-water"])
+		//		log.Infof("Water ranges to process %v", next)
+		//	}
+
+		//	for _, sr := range next {
+		//		next = compareRanges(sr, rangeMap["water-to-light"])
+		//		log.Infof("Light ranges to process %v", next)
+		//	}
+
+		//	for _, sr := range next {
+		//		next = compareRanges(sr, rangeMap["light-to-temperature"])
+		//		log.Infof("Temperature ranges to process %v", next)
+		//	}
+
+		//	for _, sr := range next {
+		//		next = compareRanges(sr, rangeMap["temperature-to-humidity"])
+		//		log.Infof("Humidity ranges to process %v", next)
+		//	}
+
+		//	for _, sr := range next {
+		//		next = compareRanges(sr, rangeMap["humidity-to-location"])
+		//		log.Infof("Location ranges %v", next)
+		//		for _, r := range next {
+		//			if r[0] < result {
+		//				result = r[0]
+		//			}
+		//		}
+		//	}
+		// }
 	}
 }
 
--- calendar/day5_test.go
+++ calendar/day5_test.go
@@ -10,7 +10,44 @@ import (
 )
 
 var _ = Describe("Day5", func() {
-	It("part 1 should be correct", func() {
+// 	It("part 1 should be correct", func() {
+// 		input := bufio.NewScanner(strings.NewReader(`seed-to-soil map:
+// 50 98 2
+// 52 50 48
+
+// soil-to-fertilizer map:
+// 0 15 37
+// 37 52 2
+// 39 0 15
+
+// fertilizer-to-water map:
+// 49 53 8
+// 0 11 42
+// 42 0 7
+// 57 7 4
+
+// water-to-light map:
+// 88 18 7
+// 18 25 70
+
+// light-to-temperature map:
+// 45 77 23
+// 81 45 19
+// 68 64 13
+
+// temperature-to-humidity map:
+// 0 69 1
+// 1 0 69
+
+// humidity-to-location map:
+// 60 56 37
+// 56 93 4
+// `))
+// 		result := 35
+// 		Expect(calendar.Day5(input, 1)).To(Equal(result))
+// 	})
+
+	It("part 2 should be correct", func() {
 		input := bufio.NewScanner(strings.NewReader(`seed-to-soil map:
 50 98 2
 52 50 48
@@ -43,13 +80,8 @@ humidity-to-location map:
 60 56 37
 56 93 4
 `))
-		result := 35
-		Expect(calendar.Day5(input, 1)).To(Equal(result))
-	})
 
-	It("part 2 should be correct", func() {
-		input := bufio.NewScanner(strings.NewReader(``))
-		result := 0
+		result := 46
 		Expect(calendar.Day5(input, 2)).To(Equal(result))
 	})
 })
